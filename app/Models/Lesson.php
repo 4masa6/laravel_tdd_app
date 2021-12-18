c076d00f7dbb4a78e89da7e1994b3f9558108972
mogata <ms46.dev@gmail.com>
2021-12-18 15:43:15 +0900

    refactoring）Reservation の HasMany を返す reservations() メソッドを追加

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lesson extends Model
 {
+    public function reservations() {
+        return $this->hasMany(Reservation::class);
+    }
+
     public function getVacancyLevelAttribute() {
         return new VacancyLevel($this->remainingCount());
     }
