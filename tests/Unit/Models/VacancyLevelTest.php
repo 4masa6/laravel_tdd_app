82ad74aedaa4a145e650c089ee463778cd8397a7
mogata <ms46.dev@gmail.com>
2021-12-18 11:45:10 +0900

    red）Slugメソッドのテスト記述

--- tests/Unit/Models/VacancyLevelTest.php
+++ tests/Unit/Models/VacancyLevelTest.php
@@ -9,11 +9,12 @@ class VacancyLevelTest extends TestCase
 {
 
     /**
+     * @test
      * @param int $remainingCount
      * @param string $expectedMark
      * @dataProvider dataMark
      */
-    public function testMark(int $remainingCount, string $expectedMark) {
+    public function Mark(int $remainingCount, string $expectedMark) {
 
         // VacancyLevelのmark()メソッドは、コンストラクタで空き枠数を受け取り、枠数に応じた記号を返す
         // 空き0 => ×, 5未満 => △, 5以上 => ◎
@@ -21,6 +22,18 @@ class VacancyLevelTest extends TestCase
         $level = new VacancyLevel($remainingCount);
         $this->assertSame($expectedMark, $level->mark());
 
+    }
+
+    /**
+     * @test
+     * @param int $remainingCount
+     * @param string $expectedSlug
+     * @dataProvider dataSlug
+     */
+    public function Slug(int $remainingCount, string $expectedSlug) {
+
+        $level = new VacancyLevel(0);
+        $this->assertSame('empty', $level->slug());
     }
 
     // PHPUnitには"dataProvider"という機能があり、テストのパターンを配列で定義してテストメソッドに渡すことができる
@@ -39,6 +52,23 @@ class VacancyLevelTest extends TestCase
             '空き十分' => [
                 'remainingCount' => 5,
                 'expectedMark' => '◎',
+            ],
+        ];
+    }
+
+    public function dataSlug() {
+        return [
+            '空きなし' => [
+                'remainingCount' => 0,
+                'expectedMark' =>  'empty',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => 'few',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => 'enough',
             ],
         ];
     }
