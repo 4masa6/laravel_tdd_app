3a97c0a98ff787c5319a5ec04cf04a577a394a48
mogata <ms46.dev@gmail.com>
2021-12-18 11:16:00 +0900

    Test/TestCaseの役割と継承の説明を記述

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    // Facadeやconfigファイルを読み込んでいる。⇒ Tests/TestCaseを継承することでLaravelの内部の機能にアクセスできるようになる。
     use CreatesApplication;
 }
